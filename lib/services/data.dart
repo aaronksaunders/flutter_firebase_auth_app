6c68960bafd163dc2832fa0655c81de3ea8ce008
aaron k saunders <aaronksaunders@gmail.com>
2019-05-26 21:02:53 -0400

    exit if there is no user

--- lib/services/data.dart
+++ lib/services/data.dart
@@ -1,5 +1,6 @@
 // import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'dart:async';
 import '../model/Item.dart';
 
@@ -8,33 +9,32 @@ class DataService {
   final Firestore _db = Firestore.instance;
 
   Stream<List<Item>> getItemsSnapshot() {
-    return _db.collection('items').snapshots().map(
-        (list) => list.documents.map((doc) => Item.fromSnap(doc)).toList());
+
+    try {
+      return _db.collection('items').snapshots().map(
+          (list) => list.documents.map((doc) => Item.fromSnap(doc)).toList());
+    } catch (e) {
+      return Stream.empty();
+    }
   }
 
   Future<Item> getItemById(String itemId) async {
-    var item = await _db
-        .collection('items')
-        .document(itemId)
-        .get();
+    var item = await _db.collection('items').document(itemId).get();
 
-        if (item.exists != null) {
-          return Future( () => Item.fromSnap(item));
-        } else {
-          return Future<Item>.value(null);
-        }
+    if (item.exists != null) {
+      return Future(() => Item.fromSnap(item));
+    } else {
+      return Future<Item>.value(null);
+    }
   }
 
   Future<ItemOwner> getUserById(String userId) async {
-    var itemOwner = await _db
-        .collection('users')
-        .document(userId)
-        .get();
+    var itemOwner = await _db.collection('users').document(userId).get();
 
-        if (itemOwner.exists != null) {
-          return Future( () => ItemOwner.fromSnap(itemOwner));
-        } else {
-          return Future<ItemOwner>.value(null);
-        }
+    if (itemOwner.exists != null) {
+      return Future(() => ItemOwner.fromSnap(itemOwner));
+    } else {
+      return Future<ItemOwner>.value(null);
+    }
   }
 }
