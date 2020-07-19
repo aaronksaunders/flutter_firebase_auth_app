88c22a6d0e45f58a605bdbd6d7313ebda3a1d4fe
Alessandro Barbosa <barbosa.alessandro@gmail.com>
2020-07-19 13:13:38 -0300

    Add files via upload
    ItemOwner supressed from the design.

--- lib/model/Item.dart
+++ lib/model/Item.dart
@@ -64,42 +64,42 @@ class Item {
   }
 }
 
-class ItemOwner {
-  String email;
-  String firstName;
-  String lastName;
-  String id;
+//class ItemOwner {
+//  String email;
+//  String firstName;
+//  String lastName;
+//  String id;
 
-  ItemOwner({this.email, this.firstName, this.lastName, this.id});
+//  ItemOwner({this.email, this.firstName, this.lastName, this.id});
 
-  factory ItemOwner.fromSnap(DocumentSnapshot itemSnap) {
-    return ItemOwner(
-        email: itemSnap.data['email'],
-        firstName: itemSnap.data['firstName'],
-        lastName: itemSnap.data['lastName'],
-        id: itemSnap.documentID ?? null);
-  }
+//  factory ItemOwner.fromSnap(DocumentSnapshot itemSnap) {
+//    return ItemOwner(
+//        email: itemSnap.data['email'],
+//        firstName: itemSnap.data['firstName'],
+//        lastName: itemSnap.data['lastName'],
+//        id: itemSnap.documentID ?? null);
+//  }
 
-  // Future saveItem() async {
-  //   var user = await FirebaseAuth.instance.currentUser();
-  //   if (user == null) {
-  //     return new Future(null);
-  //   }
-  //   var itemMap = {
-  //     'content': {
-  //       'dueDate': this.dueDate,
-  //       'body': this.body,
-  //       'subject': this.subject
-  //     },
-  //     'created': new DateTime.now().millisecondsSinceEpoch,
-  //     'updated': new DateTime.now().millisecondsSinceEpoch,
-  //   };
-  //   itemMap['owner'] = user.uid;
-  //   var response = Firestore.instance.collection('items').add(itemMap);
-  //   return response;
-  // }
+// Future saveItem() async {
+//   var user = await FirebaseAuth.instance.currentUser();
+//   if (user == null) {
+//     return new Future(null);
+//   }
+//   var itemMap = {
+//     'content': {
+//       'dueDate': this.dueDate,
+//       'body': this.body,
+//       'subject': this.subject
+//     },
+//     'created': new DateTime.now().millisecondsSinceEpoch,
+//     'updated': new DateTime.now().millisecondsSinceEpoch,
+//   };
+//   itemMap['owner'] = user.uid;
+//   var response = Firestore.instance.collection('items').add(itemMap);
+//   return response;
+// }
 
-  // Future deleteItem() {
-  //   return Firestore.instance.collection('items').document(this.id).delete();
-  // }
-}
+// Future deleteItem() {
+//   return Firestore.instance.collection('items').document(this.id).delete();
+// }
+//}
